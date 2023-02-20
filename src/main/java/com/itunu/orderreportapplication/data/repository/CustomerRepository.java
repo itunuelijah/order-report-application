0aefdffbfa3567b88cc39221fb9f6a844de257a8
itunuelijah <itunuelijah@gmail.com>
2023-02-20 02:38:07 +0100

    report application

--- src/main/java/com/itunu/orderreportapplication/data/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itunu.orderreportapplication.data.repository;
-
-import com.itunu.orderreportapplication.data.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
